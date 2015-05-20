7bfcb1b52f01b1a7351cf035f9ce4d47ed44f51d
Ciao Chow <ciaochow15@zoho.com>
2015-05-20 17:33:47 -0400

    test for addbutton

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
-jquery
+$(document).ready(function() {
+
+// Add Grocery Item //
+  $('.addbutton').on('click', function() {
+  	 var groceryitem = $('.groceryitem').val();
+  	 console.log(groceryitem);
+  	 $('.grocerylist ul').prepend('<li>' + groceryitem + '</li>');
+  });
+
+
+
+
+});
